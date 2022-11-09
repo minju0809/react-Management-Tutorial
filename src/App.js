b023c88ebcf452fdbf6d617374dc79ea1cae84a2
minju0809 <loverminju88@naver.com>
2022-11-09 22:19:40 +0900

    고객 컴포넌트 구조화

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import './App.css';
-import Customer from './components/Customer';
+import "./App.css";
+import Customer from "./components/Customer";
 
-const customer = {
-  'name': "홍길동",
-  "birthday": "910000",
-  "gender": "남자",
-  "job": "대학생"
-}
+const customers = [
+  {
+    id: 1,
+    // 랜덤으로 이미지를 보여주는 기능을 수행하는 웹사이트, 크기 64*64
+    image: "https://placeimg.com/64/64/1",
+    name: "가나다",
+    birthday: "910000",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "라마바",
+    birthday: "910000",
+    gender: "남자",
+    job: "프로그래머",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "사아자",
+    birthday: "910000",
+    gender: "남자",
+    job: "디자이너",
+  },
+];
 
 function App() {
   return (
-    <Customer 
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-    />
+    <div>
+      {customers.map((c) => {
+        return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />;
+      })}
+    </div>
   );
 }
 
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,12 +1,33 @@
 import React from "react";
 
+const CustomerProfile = (props) => {
+  return (
+    <div>
+      <img src={props.image} alt="profile" />
+      <h2>
+        {props.name}({props.id})
+      </h2>
+    </div>
+  );
+};
+
+const CustomerInfo = (props) => {
+  return (
+    <div>
+      <p>{props.birthday}</p>
+      <p>{props.gender}</p>
+      <p>{props.job}</p>
+    </div>
+  );
+};
+
 const Customer = (props) => {
-  return <div>
-    <h2>{props.name}</h2>
-    <p>{props.birthday}</p>
-    <p>{props.gender}</p>
-    <p>{props.job}</p>
-  </div>;
+  return (
+    <div>
+      <CustomerProfile id={props.id} image={props.image} name={props.name} />
+      <CustomerInfo birthday={props.birthday} gender={props.gender} job={props.job} />
+    </div>
+  );
 };
 
 export default Customer;
